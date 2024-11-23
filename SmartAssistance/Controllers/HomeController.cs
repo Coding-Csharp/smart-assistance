10d4d46ba524e2f93c1c4ee9f57a0ccb0bf07869
aaron acuña <aaronacu_85@hotmail.com>
2024-11-23 15:22:15 -0500

    Agregar archivos de proyecto.

---
+++ SmartAssistance/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartAssistance.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Error() => View();
+    }
+}
