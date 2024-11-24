c7b193c13a46c2c11e3940fe1ea01968ae2bfeff
aaron acuña <aaronacu_85@hotmail.com>
2024-11-24 16:40:50 -0500

    feat(project): added constructor in assist model.

--- SmartAssistance/Models/Assist.cs
+++ SmartAssistance/Models/Assist.cs
@@ -10,5 +10,25 @@
         public string EmotionalState { get; set; } = null!;
 
         public virtual Employee Employee { get; } = null!;
+
+        public Assist()
+        {
+            this.EmployeesId = string.Empty;
+            this.CheckIn = null;
+            this.CheckOut = null;
+            this.MinuteLate = null;
+            this.EmotionalState = string.Empty;
+        }
+        public Assist
+            (string employeeId, DateTime? checkIn,
+            DateTime? checkOut, int? minuteLate,
+            string emotionalState)
+        {
+            this.EmployeesId = employeeId;
+            this.CheckIn = checkIn;
+            this.CheckOut = checkOut;
+            this.MinuteLate = minuteLate;
+            this.EmotionalState = emotionalState;
+        }
     }
 }
