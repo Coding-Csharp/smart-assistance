e8d52e29ed524e5b289410da809e100746e055b4
aaron acuña <aaronacu_85@hotmail.com>
2024-11-24 16:34:18 -0500

    feat(admins): added admin controller with interface admin view.

---
+++ SmartAssistance/Controllers/AdminsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartAssistance.Models;
+using System.Security.Claims;
+
+namespace SmartAssistance.Controllers
+{
+    public class AdminsController
+        (SmartAssistanceContext context) :
+        Controller
+    {
+        private ClaimsPrincipal? _claimsPrincipal;
+
+        #region Views
+
+        [HttpGet]
+        public async Task<IActionResult> InterfaceAdmin()
+        {
+            ViewBag.Admin = await
+                (from em in context.Set<Employee>()
+                 join es in context.Set<Specialty>()
+                 on em.SpecialtiesId equals es.Id
+                 join an in context.Set<Assign>()
+                 on em.Id equals an.EmployeesId
+                 join po in context.Set<Position>()
+                 on an.PositionsId equals po.Id
+                 join ar in context.Set<Area>()
+                 on po.AreasId equals ar.Id
+                 where em.Id == GetPersonId()
+                 select new
+                 {
+                     em.Id,
+                     em.DateEntry,
+                     em.TypeDocument,
+                     em.Firstname,
+                     em.Lastname,
+                     em.Birthdate,
+                     em.Nationality,
+                     em.Genre,
+                     em.Phone,
+                     em.Email,
+                     em.Address,
+                     Area = ar.Name,
+                     Position = po.Name,
+                     Specialty = es.Name,
+                 }
+                ).FirstOrDefaultAsync();
+
+            return View();
+        }
+
+        #endregion
+
+        #region Json
+
+
+
+        #endregion
+
+        #region Cookie
+
+        private string GetPersonId()
+        {
+            _claimsPrincipal = HttpContext.User;
+
+            return _claimsPrincipal
+                .FindFirst(ClaimTypes.Name)?
+                .Value.ToString() ?? string.Empty;
+        }
+
+        #endregion
+    }
+}
