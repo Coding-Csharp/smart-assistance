e13ad2b3f2f1f0efe36bc74289c755f1bac960db
aaron acuña <aaronacu_85@hotmail.com>
2024-11-24 16:44:04 -0500

    feat(project): added constructor in employee credential model.

--- SmartAssistance/Models/EmployeeCredential.cs
+++ SmartAssistance/Models/EmployeeCredential.cs
@@ -6,5 +6,17 @@
         public string Code { get; set; } = null!;
 
         public virtual Employee Employee { get; set; } = null!;
+
+        public EmployeeCredential()
+        {
+            this.EmployeesId = string.Empty;
+            this.Code = string.Empty;
+        }
+        public EmployeeCredential
+            (string employeeId, string code)
+        {
+            this.EmployeesId = employeeId;
+            this.Code = code;
+        }
     }
 }
