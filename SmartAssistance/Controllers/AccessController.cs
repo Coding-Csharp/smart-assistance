287e483a061978cf61d52fb902f41840244267a2
aaron acuña <aaronacu_85@hotmail.com>
2024-11-24 16:21:36 -0500

    feat(access): added method for get information of the cookie.

--- SmartAssistance/Controllers/AccessController.cs
+++ SmartAssistance/Controllers/AccessController.cs
@@ -2,12 +2,22 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using SmartAssistance.Models;
 
 namespace SmartAssistance.Controllers
 {
     [AllowAnonymous]
-    public class AccessController : Controller
+    public class AccessController
+        (SmartAssistanceContext context,
+        IWebHostEnvironment webHostEnvironment) :
+        Controller
     {
+        private ClaimsPrincipal? _claimsPrincipal;
+        private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
+
         [HttpGet]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Login()
@@ -32,6 +42,23 @@ namespace SmartAssistance.Controllers
                 .AuthenticationScheme);
 
             return RedirectToAction("Login", "Access");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetInformation()
+        {
+            _claimsPrincipal = HttpContext.User;
+
+            var personId = _claimsPrincipal
+                .FindFirst(ClaimTypes.Name)?
+                .Value.ToString() ?? string.Empty;
+
+            var employee = await context.Set<Employee>()
+                .Where(a => a.Id == personId)
+                .FirstOrDefaultAsync();
+
+            return Content(JsonConvert.SerializeObject
+                (employee), "application/json");
         }
     }
 }
