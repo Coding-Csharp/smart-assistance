ead9b94bd6ad7328498bf17ceab74a38db8b728f
aaron acuña <aaronacu_85@hotmail.com>
2024-11-24 16:17:39 -0500

    feat(access): added access controller.

---
+++ SmartAssistance/Controllers/AccessController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartAssistance.Controllers
+{
+    [AllowAnonymous]
+    public class AccessController : Controller
+    {
+        [HttpGet]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated is true)
+            {
+                if (User.IsInRole("ADMINISTRADOR"))
+                    return RedirectToAction("InterfaceAdmin", "Admins");
+
+                else if (User.IsInRole("TRABAJADOR"))
+                    return RedirectToAction("InterfaceEmployee", "Employees");
+            }
+
+            return View();
+        }
+    }
+}
