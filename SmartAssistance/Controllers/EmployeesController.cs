2e28a1ba70f6bbc0e47f7b60cd2d54c6b5148932
aaron acuña <aaronacu_85@hotmail.com>
2024-11-26 17:24:12 -0500

    feat(employees): added interface employee view.

--- SmartAssistance/Controllers/EmployeesController.cs
+++ SmartAssistance/Controllers/EmployeesController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using SmartAssistance.Models;
 using System.Security.Claims;
+using SmartAssistance.Models;
 
 namespace SmartAssistance.Controllers
 {
